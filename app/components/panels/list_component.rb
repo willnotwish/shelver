1978711ecb5d91f91abf876969c9eeee44be2607
Nick Adams <nick.adams.software@gmail.com>
2023-05-03 11:02:12 +0100

    Removed unneded component

--- app/components/panels/list_component.rb
+++ app/components/panels/list_component.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
-class Panels::ListComponent < ViewComponent::Base
-  attr_reader :panels, :name
+module Panels
+  class ListComponent < ViewComponent::Base
+    attr_reader :panels, :name
 
-  def initialize(panels:, name:, show_details: true, show_quantified: true)
-    @panels = panels
-    @name = name
-    @show_details = show_details
-    @show_quantified = show_quantified
-  end
+    def initialize(panels:, name:, show_details: true, show_quantified: true)
+      @panels = panels
+      @name = name
+      @show_details = show_details
+      @show_quantified = show_quantified
+    end
 
-  def show_details?
-    @show_details
-  end
+    def show_details?
+      @show_details
+    end
 
-  def show_quantified?
-    @show_quantified
+    def show_quantified?
+      @show_quantified
+    end
   end
 end
